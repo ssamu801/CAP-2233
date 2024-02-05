1a6561608ed3f2c3cb79c4b13f9842254b02b059
Zay <maezy_manuel@dlsu.edu.ph>
2024-02-05 21:22:55 +0800

    db connection

--- Discussion Forums/connect.php
+++ Discussion Forums/connect.php
@@ -1,4 +1,4 @@
 <?php
-    $conn = mysqli_connect("localhost", "root") or die ("Unable to connect!". mysqli_error($conn));
-    mysqli_select_db($conn, "forum_db");
+    $conn = mysqli_connect("bq8wcg8hbi5un2pzumiu-mysql.services.clever-cloud.com", "ulxdvrgmfiqvaaax", "3ShgR8NouzOIdz4ItawK") or die ("Unable to connect!". mysqli_error($conn));
+    mysqli_select_db($conn, "bq8wcg8hbi5un2pzumiu");
 ?>
