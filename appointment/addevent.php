78ab8f8514c077bd6f6d581a3478f4140f2a516f
CASING4L <158057013+CASING4L@users.noreply.github.com>
2024-03-28 14:24:11 +0800

    Update addevent.php

--- appointment/addevent.php
+++ appointment/addevent.php
@@ -6,44 +6,29 @@ $postData = $statusMsg = $valErr = '';
 $status = 'danger'; 
  
 // If the form is submitted 
-if($_POST['Accept'] == "Accept"){ 
+if(isset($_POST['Accept'])){ 
      
     // Get event info 
     $_SESSION['postData'] = $_POST; 
-    $userID = !empty($_POST['id'])?trim($_POST['id']):''; 
-    $title = !empty($_POST['title'])?trim($_POST['title']):''; 
-    $description = !empty($_POST['description'])?trim($_POST['description']):''; 
-    $location = !empty($_POST['location'])?trim($_POST['location']):''; 
-    $date = !empty($_POST['date'])?trim($_POST['date']):''; 
-    $time_from = !empty($_POST['time_from'])?trim($_POST['time_from']):''; 
-    $time_to = !empty($_POST['time_to'])?trim($_POST['time_to']):''; 
-    $email = !empty($_POST['attendees'])?trim($_POST['attendees']):''; 
-     
-    // Validate form input fields 
-    if(empty($title)){ 
-        $valErr .= 'Please enter event title.<br/>'; 
-    } 
-    if(empty($date)){ 
-        $valErr .= 'Please enter event date.<br/>'; 
-    } 
+    $userID = !empty($_POST['userID'])?trim($_POST['userID']):''; 
      
     // Check whether user inputs are empty 
     if(empty($valErr)){ 
         // Insert data into the database 
         $sqlQ = "UPDATE events SET status=? WHERE id =?;"; 
         $stmt = $conn->prepare($sqlQ); 
-        $stmt->bind_param("ss", $db_status, $db_userID); 
+        $stmt->bind_param("si", $db_status, $db_userID); 
         $db_status = "Accepted";
         $db_userID = $userID; 
         $insert = $stmt->execute(); 
-        
         if($insert){ 
-            $event_id = $stmt->insert_id; 
+            $event_id = $db_userID; 
              
             unset($_SESSION['postData']); 
              
             // Store event ID in session 
-            $_SESSION['last_event_id'] = $event_id; 
+             $_SESSION['last_event_id'] = $event_id; 
+             
             header("Location: $googleOauthURL"); 
             exit(); 
         }else{ 
@@ -52,13 +37,21 @@ if($_POST['Accept'] == "Accept"){
     }else{ 
         $statusMsg = '<p>Please fill all the mandatory fields:</p>'.trim($valErr, '<br/>'); 
     } 
-}else if($_POST['Reject'] == "Reject"){ 
-    $statusMsg = 'Form submission failed!';
-    header("Location: profile.php"); 
+}else if(isset($_POST['Reject'])){ 
+    $_SESSION['postData'] = $_POST; 
+    $userID = !empty($_POST['userID'])?trim($_POST['userID']):''; 
+
+    $sqlQ = "UPDATE events SET status=? WHERE id =?;"; 
+    $stmt = $conn->prepare($sqlQ); 
+    $stmt->bind_param("si", $db_status, $db_userID); 
+    $db_status = "Rejected";
+    $db_userID = $userID; 
+    $insert = $stmt->execute();
+
+    header("Location: pendingrequests.php"); 
 } 
  
 $_SESSION['status_response'] = array('status' => $status, 'status_msg' => $statusMsg); 
  
-header("Location: index.php"); 
 exit(); 
 ?>
