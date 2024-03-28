90c5380125a29bf3e90e396e3473230ce8bfacdf
CASING4L <158057013+CASING4L@users.noreply.github.com>
2024-03-28 14:21:20 +0800

    Update pendingrequests.php

--- appointment/pendingrequests.php
+++ appointment/pendingrequests.php
@@ -11,7 +11,7 @@
 <body style="background-color:#006937">
 
 
-    <div style='width:50%; margin-left: auto; margin-right: auto; padding-top: 250px; padding-bottom: 100px'>
+    <div style='width:50%; margin-left: 300px; padding-top: 250px; padding-bottom: 100px'>
         <table class='table table-hover table-striped table-dark'>
             <thead>
                 <tr>
@@ -27,29 +27,44 @@
      	include("connect.php");
 
          $sql = "
-         SELECT * FROM events";
+         SELECT * FROM events WHERE status = 'Pending'";
          
         $records = mysqli_query($conn, $sql) or die(mysqli_error($conn));
- 
-        while ($results = mysqli_fetch_array($records)) {
-            if($results['status'] == "pending"){
-                echo"
-                <tbody>
-                    <tr>
-                    <form method='get' action='addevent.php'>  
-                         <input type='hidden' id='custId' name='custId' value=$results[id]>
-                        <td>$results[email]</td>
-                        <td>$results[title]</td>
-                        <td>$results[date]</td>
-                        <td>$results[time_from]</td>
-                        <td>$results[time_to]</td>
-                        <td><input type='submit' name='Accept' value='Accept' /></td>
-                        <td><input type='submit' name='Reject' value='Reject' /></td>
-                    </tr>
-                </tbody>
-                ";
-                }
-        } 	
+        $total = mysqli_num_rows($records);
+
+        if($total > 0){
+            while ($results = mysqli_fetch_array($records)) {
+
+                if($results['status'] == "Pending"){
+                    echo"
+                    <tbody>
+                        <tr>
+                            <form method='post' action='addevent.php'>  
+                                <input type='hidden' id='userID' name='userID' value=$results[id]>
+                                <td><input type='text' value='$results[user_email]' name='email' readonly></td>
+                                <td><input type='text' value='$results[counselor_email]' name='email' readonly></td>
+                                <td><input type='text' value='$results[title]' name='title' readonly></td>
+                                <td><input type='text' value='$results[date]' name='date' readonly></td>
+                                <td><input type='text' value='$results[time_from]' name='time_from' readonly></td>
+                                <td><input type='text' value='$results[time_to]' name='time_to' readonly></td>
+                                <td><input type='submit' name='Accept' value='Accept'/></td>
+                                <td><input type='submit' name='Reject' value='Reject'/></td>
+                            </form>
+                        </tr>
+                    </tbody>
+                    ";
+                    } 
+            } 
+        } else {
+            echo"
+            <tbody>
+                <tr>
+                <td colspan='5' style='text-align:center;'>There are currently no pending requests</td>
+                </tr>
+            </tbody>
+            ";
+        }
+	
         ?>
         </table>
     </div>
