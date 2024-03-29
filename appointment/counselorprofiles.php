503c7d9998b17549129582a9b00fc997d27c0ed2
CASING4L <158057013+CASING4L@users.noreply.github.com>
2024-03-29 22:15:15 +0800

    Add files via upload

---
+++ appointment/counselorprofiles.php
@@ -0,0 +1,55 @@
+<html>
+<head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.0/dist/css/bootstrap.min.css">    
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+	<title>Counselors</title>
+</head>
+<body style="background-color:#006937">
+<div class="card" style="width: 18rem; margin-left: 1rem; margin-right: 1rem">
+  <img src="counselor1.png" tyle="height: 260px; width: 285px" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h4 class="card-title">Eric Johnson</h4>
+    <p><i>Counselor</i></p>
+    <p><b>Schedule:</b><br>Mondays & Thursdays<br>  10am - 4 pm</p>
+    <!-- <a href="#" class="btn btn-primary">View Profile</a> -->
+  </div>
+</div>
+
+<div class="card" style="width: 18rem; margin-left: 1rem; margin-right: 1rem">
+  <img src="counselor2.png" style="height: 260px; width: 285px" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h4 class="card-title">James Urban</h4>
+    <p><i>Counselor</i></p>
+    <p><b>Schedule:</b><br>Wednesdays & Saturdays<br>  10am - 6 pm</p>
+    <!-- <a href="#" class="btn btn-primary">View Profile</a> -->
+  </div>
+</div>
+
+<div class="card" style="width: 18rem; margin-left: 1rem; margin-right: 1rem">
+  <img src="counselor3.jpeg" style="height: 260px; width: 285px" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h4 class="card-title">Ellen Smith</h4>
+    <p><i>Counselor</i></p>
+    <p><b>Schedule:</b><br>Tuesdays & Fridays<br>  8am - 3 pm</p>
+    <!-- <a href="#" class="btn btn-primary">View Profile</a> -->
+  </div>
+</div>
+
+<div class="card" style="width: 18rem; margin-left: 1rem; margin-right: 1rem">
+  <img src="counselor4.png" style="height: 260px; width: 285px" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h4 class="card-title">Jenna Jones</h4>
+    <p><i>Counselor</i></p>
+    <p><b>Schedule:</b><br>Mondays & Thursdays<br>  9am - 5 pm</p>
+    <!-- <a href="#" class="btn btn-primary">View Profile</a> -->
+  </div>
+</div>
+   
+		
+</body>
+</html>
