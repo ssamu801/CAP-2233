2f75fa792f50de3fc076c5de39c7167e3d7189bb
saiko915 <127884452+saiko915@users.noreply.github.com>
2024-05-03 07:12:58 +0800

    FAQs, Resources, and Crisis
    Added FAQs page (+ FAQ submenu on navbar); Advanced Search frontend on resources; Search function on Crisis Directory (unfinished)

---
+++ Rochelle/faqs.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="css/resourcesstyle.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"rel="stylesheet">  
+
+    <style>
+        .card-body {
+            background-color: white;
+        }
+
+        .items {
+            padding: 10px;
+        }
+
+        .items .card {
+            background-color: green;
+        }
+
+        .btn-link {
+            text-decoration: none;
+            color: white;
+            font-weight: 700;
+            font-size: 20px;
+        }
+    </style>
+
+</head>
+<body>
+    <div id="wrapper-container" class="wrapper-container">
+        <div class="container-pusher">
+            <div class="content-pusher">
+                <header id="head" class="site-header">
+                    <section class="waspchat">
+                        <div class="waspchat-contianer">
+                            <!-- link to real chat -->
+                            <a href="" title="OCCS - WASP" target="_blank">
+                                <span class="waspchat-text" style="text-decoration: underline;">
+                                    WASP Real-Time Chat Support
+                                </span>
+                            </a>
+                        </div>
+                    </section>
+                </header>
+                <div id="main-content">
+                    <section class="content-area">
+                        <div class="top_heading  _out">
+                            <div class="top_site_main" style="background-image: url(assets/img/DLSUbg.jpg);"></div>
+                        </div>
+                        <div class="site-content">
+                            <main id="main">
+                                <div class="content-wrapper">
+                                    <section class="wrapper">
+                                        <h2>
+                                            <span style="color: green; font-size: 30px; font-weight: 700; font-style: normal;">
+                                                <strong>Frequently Asked Questions</strong>
+                                            </span>
+                                        </h2>
+                                    </section>
+                                    <hr>
+                                    <div class="items" id="accordion">
+                                        <div class="card">
+                                            <div class="card-header" id="headingOne">
+                                                <h5 class="mb-0">
+                                                    <button class="btn btn-link" data-toggle="collapse" data-target="#collapse1" aria-expanded="false" aria-controls="collapse1">
+                                                    Schedule an Appointment
+                                                    </button>
+                                                </h5>
+                                            </div>
+                                            <div id="collapse1" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
+                                                <div class="card-body">
+                                                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div class="card">
+                                            <div class="card-header" id="headingTwo">
+                                                <h5 class="mb-0">
+                                                    <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapse2" aria-expanded="false" aria-controls="collapse2">
+                                                    Services and Programs
+                                                    </button>
+                                                </h5>
+                                            </div>
+                                            <div id="collapse2" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+                                                <div class="card-body">
+                                                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div class="card">
+                                            <div class="card-header" id="headingThree">
+                                                <h5 class="mb-0">
+                                                    <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapse3" aria-expanded="false" aria-controls="collapse3">
+                                                    Online Resources
+                                                    </button>
+                                                </h5>
+                                            </div>
+                                            <div id="collapse3" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+                                                <div class="card-body">
+                                                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div class="card">
+                                            <div class="card-header" id="headingThree">
+                                                <h5 class="mb-0">
+                                                    <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapse4" aria-expanded="false" aria-controls="collapse4">
+                                                    OCCS Counselors
+                                                    </button>
+                                                </h5>
+                                            </div>
+                                            <div id="collapse4" class="collapse" aria-labelledby="headingFour" data-parent="#accordion">
+                                                <div class="card-body">
+                                                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </main>
+                        </div>
+                    </section>
+                </div>
+            </div>
+        </div>
+    </div>                           
+</body>
+</html>
