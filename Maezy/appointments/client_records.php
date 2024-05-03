e7ad6c0e3439df3064cd1861eed3d6722534c8b8
ssamu801 <114760962+ssamu801@users.noreply.github.com>
2024-05-03 17:50:46 +0800

    Slight changes 05/02/24
    client_records.php - line 36 (u.id=e.student_id)
    session_records.php - line 17 (u.id=e.student_id)

---
+++ Maezy/appointments/client_records.php
@@ -0,0 +1,72 @@
+<?php  include('./db_connect.php'); ?>
+<style>
+	.record_row:hover{
+		cursor: pointer;
+	}
+</style>
+
+<div class="container-fluid">
+<div class="row mb-4 mt-4">
+		    <div class="col-md-12">
+				
+		    </div>
+	    </div>
+    <div class="col-lg-12">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+					<div class="card-header">
+						<b>Client Records</b>
+						<span class=""></span>
+                    </div>
+                    <div class="card-body">
+						<table class="table table-striped table-hover">
+                            <thead>
+				                <tr>
+					                <th class="text-center">ID Number</th>
+					                <th class="text-center">Name</th>
+					                <th class="text-center">Email</th>
+				                </tr>
+			                </thead>
+							<tbody>
+				                <?php
+ 					                include './db_connect.php';
+									$counselor_email = $_SESSION['login_email'];
+ 					                $type = array("","Admin","Staff","Subscriber");
+ 					                $users = $conn->query("SELECT u.id, u.name, u.email FROM users u JOIN events e ON u.id=e.student_id WHERE e.counselor_email ='$counselor_email' GROUP BY u.id ORDER BY u.name ASC");
+ 					                $i = 1;
+ 					                while($row= $users->fetch_assoc()):
+				                ?>
+				                <tr class="client_record record_row" data-id="<?php echo $row['id'] ?>">
+				 	                <td class="text-center">
+				 		                <?php echo $row['id'] ?>
+				 	                </td>
+				 	                <td class="text-center">
+				 		                <?php echo ucwords($row['name']) ?>
+				 	                </td>
+				 	
+				 	                <td class="text-center">
+				 		                <?php echo $row['email'] ?>
+				 	                </td>
+
+				                </tr>
+				            <?php endwhile; ?>
+			            </tbody>
+					</table>
+					</div>    
+                </div>         
+            </div>     
+        </div>   
+    </div>
+</div>
+<script>
+
+$('.client_record').click(function(){
+    var dataId = $(this).attr('data-id');
+    var clientName = $(this).find('td:eq(1)').text(); // Assuming the name is in the second column (index 1)
+    view_modal("Client Record: " + clientName + " (ID: " + dataId + ")", "appointments/session_records.php?id=" + dataId, 'large');	
+});
+$('table').dataTable();
+
+
+</script>
---
+++ Maezy/appointments/session_records.php
@@ -0,0 +1,52 @@
+<table class="table table-striped col-md-12">
+			<thead>
+				<tr>
+					<th class="text-center">Date of Availed Session</th>
+					<th class="text-center">Time of Availed Session</th>
+                    <th class="text-center">Attending Counselor</th>
+					<th class="text-center">Status</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php
+					$user_id = $_GET['id'];
+ 					include '../db_connect.php';
+ 					$type = array("","Admin","Staff","Subscriber");
+ 					$users = $conn->query("SELECT e.date, e.time_from, e.time_to, e.counselor_name, e.status 
+										   FROM users u 
+										   JOIN events e ON u.id=e.student_id 
+										   WHERE u.id= $user_id
+										   ORDER BY e.date DESC");
+ 					$i = 1;
+ 					while($row= $users->fetch_assoc()):
+				 ?>
+				 <tr>
+				 	<td class="text-center">
+					 	<?php echo $row['date'] ?>
+				 	</td>
+				 	<td class="text-center">
+					 <?php
+
+						$time_from = date("h:i A", strtotime($row['time_from']));
+						$time_to = date("h:i A", strtotime($row['time_to']));
+
+						echo $time_from . " - " . $time_to;
+					?>
+				 	</td>
+
+                     <td class="text-center">
+				 		<?php echo $row['counselor_name'] ?>
+				 	</td>
+				 	
+				 	<td class="text-center">
+				 		<?php echo $row['status'] ?>
+				 </tr>
+				<?php endwhile; ?>
+			</tbody>
+</table>
+			
+<script>
+$('table').dataTable();
+
+
+</script>
