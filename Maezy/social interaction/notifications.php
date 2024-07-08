24abcecaf72643bf7e54c5c52a12924ef073763e
ssamu801 <114760962+ssamu801@users.noreply.github.com>
2024-07-09 01:17:16 +0800

    changes

--- Maezy/social interaction/notifications.php
+++ Maezy/social interaction/notifications.php
@@ -39,96 +39,112 @@
 
                             $id =  $_SESSION['login_id'];
 							$notifs = $conn->query("SELECT * FROM notifications WHERE posterID=$id ORDER BY time DESC");
-							while($row= $notifs->fetch_assoc()):
-							?>
-							<li class="list-group-item mb-4">
-								<div>
-								<?php if($row['type'] == 1): ?>
-									<?php 
-										$heading = "";
-										$notif_id = $row['id'];
-										$sql = "SELECT t.title FROM notifications n JOIN topics t ON n.topic_id=t.id WHERE n.id=$notif_id LIMIT 1;";
-										$result = $conn->query($sql);
-										if ($result && $result->num_rows > 0) {
-											$row_title = $result->fetch_assoc();
-											$heading = "[DISCUSSION FORUM] Your post '" . $row_title['title'] . "' has been approved";
-										} 
+							
+                            // Check if there are notifications
+							if ($notifs && $notifs->num_rows > 0) {
+								while($row= $notifs->fetch_assoc()):
 									?>
-									<a href="#" class="filter-text view_notif" data-id="<?php echo $row['id'] . ' ' . $row['type']; ?>"
-   										data-heading="<?php echo htmlspecialchars($heading); ?>"
-   										style="color: #444444; font-weight: bold;"><?php echo $heading; ?>
-									</a>
-								<?php elseif($row['type'] == 2): ?>	
-									<?php 
-										$heading = "";
-										$notif_id = $row['id'];
-										$sql = "SELECT t.title FROM notifications n JOIN topics t ON n.topic_id=t.id WHERE n.id=$notif_id LIMIT 1;";
-										$result = $conn->query($sql);
-										if ($result && $result->num_rows > 0) {
-											$row_title = $result->fetch_assoc();
-											$heading = "[DISCUSSION FORUM] Your post '" . $row_title['title'] . "' has been rejected";
-										} 
-									?>
-									<a href="#" class="filter-text view_notif" data-id="<?php echo $row['id'] . ' ' . $row['type']; ?>"
-   										data-heading="<?php echo htmlspecialchars($heading); ?>"
-   										style="color: #444444; font-weight: bold;"><?php echo $heading; ?>
-									</a>
-								<?php elseif($row['type'] == 3): ?>	
-									<?php 
-										$heading = "";
-										$notif_id = $row['id'];
-										$sql = "SELECT t.title, c.comment FROM notifications n JOIN comments c ON n.comment_id=c.id JOIN topics t ON c.topic_id=t.id WHERE n.id=$notif_id LIMIT 1;";
-										$result = $conn->query($sql);
-										if ($result && $result->num_rows > 0) {
-											$row_title = $result->fetch_assoc();
-											$heading = "[DISCUSSION FORUM] Comment '" . $row_title['comment'] . "' is accepted on Forum on post '" . $row_title['title'] . "'";
-										} 
-									?>
-									<a href="#" class="filter-text view_notif" data-id="<?php echo $row['id'] . ' ' . $row['type']; ?>"
-   										data-heading="<?php echo htmlspecialchars($heading); ?>"
-   										style="color: #444444; font-weight: bold;"><?php echo $heading; ?>
-									</a>
-								<?php elseif($row['type'] == 4): ?>	
-									<?php 
-										$heading = "";
-										$notif_id = $row['id'];
-										$sql = "SELECT t.title, c.comment FROM notifications n JOIN comments c ON n.comment_id=c.id JOIN topics t ON c.topic_id=t.id WHERE n.id=$notif_id LIMIT 1;";
-										$result = $conn->query($sql);
-										if ($result && $result->num_rows > 0) {
-											$row_title = $result->fetch_assoc();
-											$heading = "[DISCUSSION FORUM] Comment '" . $row_title['comment'] . "' is not accepted on Forum on post '" . $row_title['title'] . "'";
-										} 
-									?>
-									<a href="#" class="filter-text view_notif" data-id="<?php echo $row['id'] . ' ' . $row['type']; ?>"
-   										data-heading="<?php echo htmlspecialchars($heading); ?>"
-   										style="color: #444444; font-weight: bold;"><?php echo $heading; ?>
-									</a>	
-								<?php elseif($row['type'] == 5): ?>	
-									<?php 
-										$heading = "";
-										$notif_id = $row['id'];
-										$sql = "SELECT t.title, c.comment, u.name FROM notifications n JOIN comments c ON n.comment_id=c.id JOIN topics t ON n.topic_id=t.id JOIN users u ON u.id=c.user_id  WHERE n.id=$notif_id LIMIT 1;";
-										$result = $conn->query($sql);
-										if ($result && $result->num_rows > 0) {
-											$row_title = $result->fetch_assoc();
-											$heading = "[DISCUSSION FORUM] ".$row_title['name']. " commented on your post Forum '" . $row_title['title'] . "'";
-										} 
-									?>
-									<a href="#" class="filter-text view_notif" data-id="<?php echo $row['id'] . ' ' . $row['type']; ?>"
-   										data-heading="<?php echo htmlspecialchars($heading); ?>"
-   										style="color: #444444; font-weight: bold;"><?php echo $heading; ?>
-									</a>				
-								<?php else: ?>
-									<a href="#" class="filter-text view_notif" data-id="<?php echo $row['id']; ?>"
-   										data-heading="<?php echo htmlspecialchars($row['heading']); ?>"
-   										style="color: #444444; font-weight: bold;"><?php echo $row['heading']; ?>
-									</a>
-								<?php endif; ?>
+									<li class="list-group-item mb-4">
+										<div>
+										<?php if($row['type'] == 1): ?>
+											<?php 
+												$heading = "";
+												$notif_id = $row['id'];
+												$sql = "SELECT t.title FROM notifications n JOIN topics t ON n.topic_id=t.id WHERE n.id=$notif_id LIMIT 1;";
+												$result = $conn->query($sql);
+												if ($result && $result->num_rows > 0) {
+													$row_title = $result->fetch_assoc();
+													$heading = "[DISCUSSION FORUM] Your post '" . $row_title['title'] . "' has been approved";
+												} 
+											?>
+											<a href="#" class="filter-text view_notif" data-id="<?php echo $row['id'] . ' ' . $row['type']; ?>"
+												data-heading="<?php echo htmlspecialchars($heading); ?>"
+												style="color: #444444; font-weight: bold;"><?php echo $heading; ?>
+											</a>
+										<?php elseif($row['type'] == 2): ?>	
+											<?php 
+												$heading = "";
+												$notif_id = $row['id'];
+												$sql = "SELECT t.title FROM notifications n JOIN topics t ON n.topic_id=t.id WHERE n.id=$notif_id LIMIT 1;";
+												$result = $conn->query($sql);
+												if ($result && $result->num_rows > 0) {
+													$row_title = $result->fetch_assoc();
+													$heading = "[DISCUSSION FORUM] Your post '" . $row_title['title'] . "' has been rejected";
+												} 
+											?>
+											<a href="#" class="filter-text view_notif" data-id="<?php echo $row['id'] . ' ' . $row['type']; ?>"
+												data-heading="<?php echo htmlspecialchars($heading); ?>"
+												style="color: #444444; font-weight: bold;"><?php echo $heading; ?>
+											</a>
+										<?php elseif($row['type'] == 3): ?>	
+											<?php 
+												$heading = "";
+												$notif_id = $row['id'];
+												$sql = "SELECT t.title, c.comment FROM notifications n JOIN comments c ON n.comment_id=c.id JOIN topics t ON c.topic_id=t.id WHERE n.id=$notif_id LIMIT 1;";
+												$result = $conn->query($sql);
+												if ($result && $result->num_rows > 0) {
+													$row_title = $result->fetch_assoc();
+													$heading = "[DISCUSSION FORUM] Comment '" . $row_title['comment'] . "' is accepted on Forum on post '" . $row_title['title'] . "'";
+												} 
+											?>
+											<a href="#" class="filter-text view_notif" data-id="<?php echo $row['id'] . ' ' . $row['type']; ?>"
+												data-heading="<?php echo htmlspecialchars($heading); ?>"
+												style="color: #444444; font-weight: bold;"><?php echo $heading; ?>
+											</a>
+										<?php elseif($row['type'] == 4): ?>	
+											<?php 
+												$heading = "";
+												$notif_id = $row['id'];
+												$sql = "SELECT t.title, c.comment FROM notifications n JOIN comments c ON n.comment_id=c.id JOIN topics t ON c.topic_id=t.id WHERE n.id=$notif_id LIMIT 1;";
+												$result = $conn->query($sql);
+												if ($result && $result->num_rows > 0) {
+													$row_title = $result->fetch_assoc();
+													$heading = "[DISCUSSION FORUM] Comment '" . $row_title['comment'] . "' is not accepted on Forum on post '" . $row_title['title'] . "'";
+												} 
+											?>
+											<a href="#" class="filter-text view_notif" data-id="<?php echo $row['id'] . ' ' . $row['type']; ?>"
+												data-heading="<?php echo htmlspecialchars($heading); ?>"
+												style="color: #444444; font-weight: bold;"><?php echo $heading; ?>
+											</a>	
+										<?php elseif($row['type'] == 5): ?>	
+											<?php 
+												$heading = "";
+												$notif_id = $row['id'];
+												$sql = "SELECT t.title, c.comment, u.name FROM notifications n JOIN comments c ON n.comment_id=c.id JOIN topics t ON n.topic_id=t.id JOIN users u ON u.id=c.user_id  WHERE n.id=$notif_id LIMIT 1;";
+												$result = $conn->query($sql);
+												if ($result && $result->num_rows > 0) {
+													$row_title = $result->fetch_assoc();
+													$heading = "[DISCUSSION FORUM] ".$row_title['name']. " commented on your post Forum '" . $row_title['title'] . "'";
+												} 
+											?>
+											<a href="#" class="filter-text view_notif" data-id="<?php echo $row['id'] . ' ' . $row['type']; ?>"
+												data-heading="<?php echo htmlspecialchars($heading); ?>"
+												style="color: #444444; font-weight: bold;"><?php echo $heading; ?>
+											</a>
+										<?php elseif($row['type'] == 6): ?>	
+											<?php 
+												$heading = "[APPOINTMENT] Your request has been successfully submitted";
+											?>
+											<a href="#" class="filter-text view_notif" data-id="<?php echo $row['id'] . ' ' . $row['type']; ?>"
+												data-heading="<?php echo htmlspecialchars($heading); ?>"
+												style="color: #444444; font-weight: bold;"><?php echo $heading; ?>
+											</a>					
+										<?php else: ?>
+											<a href="#" class="filter-text view_notif" data-id="<?php echo $row['id']; ?>"
+												data-heading="<?php echo htmlspecialchars($row['heading']); ?>"
+												style="color: #444444; font-weight: bold;"><?php echo $row['heading']; ?>
+											</a>
+										<?php endif; ?>
 
-								</div>
+										</div>
 
-							</li>
-						<?php endwhile; ?>
+									</li>
+								<?php endwhile; ?>
+							<?php } else { ?>
+								<li class="list-group-item mb-4">
+									You have no notifications yet.
+								</li>
+							<?php } ?>
 						</ul>
 					</div>
 				</div>
@@ -173,21 +189,11 @@
 	});
 
 
-	function delete_topic($id){
-		start_load()
-		$.ajax({
-			url:'ajax.php?action=delete_topic',
-			method:'POST',
-			data:{id:$id},
-			success:function(resp){
-				if(resp==1){
-					alert_toast("Data successfully deleted",'success')
-					setTimeout(function(){
-						location.reload()
-					},1500)
-
-				}
-			}
-		})
-	}
 </script>
+
+<?php 
+	/* 
+		42-44
+		143-147
+	*/
+?>
