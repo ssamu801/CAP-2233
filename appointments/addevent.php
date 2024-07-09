e58b92792d974f9cfaaeb5c6f342009c8161b220
ssamu801 <maezy_manuel@dlsu.edu.ph>
2024-07-09 21:03:40 +0800

    Inetgration - Karl

--- appointments/addevent.php
+++ appointments/addevent.php
@@ -1,4 +1,5 @@
 <?php     
+
 // Include database configuration file 
 require_once './db_connect.php'; 
  
@@ -11,12 +12,26 @@ if(isset($_POST['Accept'])){
     // Get event info 
     $_SESSION['postData'] = $_POST; 
     $userID = !empty($_POST['userID'])?trim($_POST['userID']):''; 
+    $student_id = !empty($_POST['student_id'])?trim($_POST['student_id']):''; 
     $counselor_email = !empty($_POST['counselor_email'])?trim($_POST['counselor_email']):''; 
     $counselor_name = !empty($_POST['counselor_name'])?trim($_POST['counselor_name']):''; 
+    $user_email = !empty($_POST['user_email'])?trim($_POST['user_email']):''; 
+    $user_name = !empty($_POST['user_name'])?trim($_POST['user_name']):''; 
     $location = !empty($_POST['location'])?trim($_POST['location']):''; 
+    $time_from = !empty($_POST['time_from'])?trim($_POST['time_from']):''; 
+    $time_to = !empty($_POST['time_to'])?trim($_POST['time_to']):''; 
+    $date = !empty($_POST['date'])?trim($_POST['date']):''; 
      
     // Check whether user inputs are empty 
     if(empty($valErr)){ 
+        
+        $sqlQ = "INSERT INTO event_notifications (id, description, user_email, counselor_name, time, event_start, location, event_end, event_date, event_status, user_name) VALUES (?, ?, ?, ?, NOW(), ?, ?, ?, ?, ?, ?)";
+        $stmt = $conn->prepare($sqlQ);
+        $stmt->bind_param("isssssssss", $student_id, $notif_desc, $user_email, $counselor_name, $time_from, $location, $time_to, $date, $event_status, $user_name);
+        $notif_desc = "Your appointment has been confirmed";
+        $event_status = "Pending";
+        $insert = $stmt->execute();
+
         // Insert data into the database 
         $sqlQ = "UPDATE events SET location=?, counselor_name=?, counselor_email=?, status=? WHERE id=?";
         $stmt = $conn->prepare($sqlQ);
@@ -75,3 +90,16 @@ exit();
         }, delay);
     }
 </script>
+
+<?php
+/* Changes as of 7/8/2024
+    Main changes: Added user email and username. Updated queries
+
+   Lines added: 
+	18-19
+	28
+	30
+	32
+
+   End of Changes*/
+?>
