a00cfa3f3ade5c4e4d67d5555a72936b01ba2857
kmcasingal <113958738+kmcasingal@users.noreply.github.com>
2024-07-14 19:47:37 +0800

    Add files via upload

--- Karl/calendarapi.class.php
+++ Karl/calendarapi.class.php
@@ -1,17 +1,17 @@
-<?php 
+<?php
 
 class GoogleCalendarApi { 
     const OAUTH2_TOKEN_URI = 'https://accounts.google.com/o/oauth2/token'; 
     const CALENDAR_TIMEZONE_URI = 'https://www.googleapis.com/calendar/v3/users/me/settings/timezone'; 
     const CALENDAR_LIST = 'https://www.googleapis.com/calendar/v3/users/me/calendarList'; 
     const CALENDAR_EVENT = 'https://www.googleapis.com/calendar/v3/calendars/'; 
-     
+    
     function __construct($params = array()) { 
         if (count($params) > 0){ 
             $this->initialize($params);         
         } 
     } 
-     
+    
     function initialize($params = array()) { 
         if (count($params) > 0){ 
             foreach ($params as $key => $val){ 
@@ -21,7 +21,7 @@ class GoogleCalendarApi {
             }         
         } 
     } 
-     
+    
     public function GetAccessToken($client_id, $redirect_uri, $client_secret, $code) { 
         $curlPost = 'client_id=' . $client_id . '&redirect_uri=' . $redirect_uri . '&client_secret=' . $client_secret . '&code='. $code . '&grant_type=authorization_code'; 
         $ch = curl_init();         
@@ -32,7 +32,7 @@ class GoogleCalendarApi {
         curl_setopt($ch, CURLOPT_POSTFIELDS, $curlPost);     
         $data = json_decode(curl_exec($ch), true); 
         $http_code = curl_getinfo($ch,CURLINFO_HTTP_CODE); 
-         
+        
         if ($http_code != 200) { 
             $error_msg = 'Failed to receieve access token'; 
             if (curl_errno($ch)) { 
@@ -40,10 +40,10 @@ class GoogleCalendarApi {
             } 
             throw new Exception('Error '.$http_code.': '.$error_msg); 
         } 
-             
+        
         return $data; 
     } 
- 
+
     public function GetUserCalendarTimezone($access_token) { 
         $ch = curl_init();         
         curl_setopt($ch, CURLOPT_URL, self::CALENDAR_TIMEZONE_URI);         
@@ -52,7 +52,7 @@ class GoogleCalendarApi {
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);     
         $data = json_decode(curl_exec($ch), true); 
         $http_code = curl_getinfo($ch,CURLINFO_HTTP_CODE); 
-         
+        
         if ($http_code != 200) { 
             $error_msg = 'Failed to fetch timezone'; 
             if (curl_errno($ch)) { 
@@ -60,18 +60,18 @@ class GoogleCalendarApi {
             } 
             throw new Exception('Error '.$http_code.': '.$error_msg); 
         } 
- 
+
         return $data['value']; 
     } 
- 
+
     public function GetCalendarsList($access_token) { 
         $url_parameters = array(); 
- 
+
         $url_parameters['fields'] = 'items(id,summary,timeZone)'; 
         $url_parameters['minAccessRole'] = 'owner'; 
- 
+
         $url_calendars = self::CALENDAR_LIST.'?'. http_build_query($url_parameters); 
-         
+        
         $ch = curl_init();         
         curl_setopt($ch, CURLOPT_URL, $url_calendars);         
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);     
@@ -79,7 +79,7 @@ class GoogleCalendarApi {
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);     
         $data = json_decode(curl_exec($ch), true); 
         $http_code = curl_getinfo($ch,CURLINFO_HTTP_CODE); 
-         
+        
         if ($http_code != 200) { 
             $error_msg = 'Failed to get calendars list'; 
             if (curl_errno($ch)) { 
@@ -87,23 +87,23 @@ class GoogleCalendarApi {
             } 
             throw new Exception('Error '.$http_code.': '.$error_msg); 
         } 
- 
+
         return $data['items']; 
     } 
- 
+
     public function CreateCalendarEvent($access_token, $calendar_id, $event_data, $all_day, $event_datetime, $event_timezone) { 
         $apiURL = self::CALENDAR_EVENT . $calendar_id . '/events'; 
-         
+        
         $curlPost = array(); 
-         
+        
         if(!empty($event_data['summary'])){ 
             $curlPost['summary'] =  $event_data['summary'];
         } 
-         
+        
         if(!empty($event_data['location'])){ 
             $curlPost['location'] = $event_data['location'];
         } 
-         
+        
         if(!empty($event_data['description'])){ 
             $curlPost['description'] = $event_data['description']; 
         } 
@@ -118,11 +118,11 @@ class GoogleCalendarApi {
         //line 118
         $curlPost['sendNotifications'] = true;
         $curlPost['reminders'] = $event_data['reminders'];
-         
+        
         $event_date = !empty($event_datetime['event_date'])?$event_datetime['event_date']:date("Y-m-d"); 
         $start_time = !empty($event_datetime['start_time'])?$event_datetime['start_time']:date("H:i:s"); 
         $end_time = !empty($event_datetime['end_time'])?$event_datetime['end_time']:date("H:i:s"); 
- 
+
         if($all_day == 1){ 
             $curlPost['start'] = array('date' => $event_date); 
             $curlPost['end'] = array('date' => $event_date); 
@@ -131,7 +131,7 @@ class GoogleCalendarApi {
             $timezone_offset = !empty($timezone_offset)?$timezone_offset:'07:00'; 
             $dateTime_start = $event_date.'T'.$start_time.$timezone_offset; 
             $dateTime_end = $event_date.'T'.$end_time.$timezone_offset; 
-             
+            
             $curlPost['start'] = array('dateTime' => $dateTime_start, 'timeZone' => $event_timezone); 
             $curlPost['end'] = array('dateTime' => $dateTime_end, 'timeZone' => $event_timezone); 
         } 
@@ -144,7 +144,7 @@ class GoogleCalendarApi {
         curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($curlPost));     
         $data = json_decode(curl_exec($ch), true); 
         $http_code = curl_getinfo($ch,CURLINFO_HTTP_CODE);         
-         
+        
         if ($http_code != 200) { 
             $error_msg = 'Failed to create event'; 
             if (curl_errno($ch)) { 
@@ -152,24 +152,39 @@ class GoogleCalendarApi {
             } 
             throw new Exception('Error '.$http_code.': '.$error_msg); 
         } 
- 
+
         return $data['id']; 
     } 
-     
+    
     private function getTimezoneOffset($timezone = 'America/Los_Angeles'){ 
         $current   = timezone_open($timezone); 
         $utcTime  = new \DateTime('now', new \DateTimeZone('UTC')); 
         $offsetInSecs =  timezone_offset_get($current, $utcTime); 
         $hoursAndSec = gmdate('H:i', abs($offsetInSecs)); 
         return stripos($offsetInSecs, '-') === false ? "+{$hoursAndSec}" : "-{$hoursAndSec}"; 
-    } 
+    }
+
+    public function DeleteCalendarEvent($access_token, $calendar_id, $event_id) { 
+        $apiURL = self::CALENDAR_EVENT . $calendar_id . '/events/' . $event_id; 
+        
+        $ch = curl_init();         
+        curl_setopt($ch, CURLOPT_URL, $apiURL);         
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);         
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");         
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE); 
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Bearer '. $access_token));     
+        $data = curl_exec($ch);
+        $http_code = curl_getinfo($ch,CURLINFO_HTTP_CODE);         
+        
+        if ($http_code != 204) { 
+            $error_msg = 'Failed to delete event'; 
+            if (curl_errno($ch)) { 
+                $error_msg = curl_error($ch); 
+            } 
+            throw new Exception('Error '.$http_code.': '.$error_msg); 
+        } 
+
+        return true; 
+    }
 } 
-?>
-
-<?php
-/* Changes as of 11:00PM - May 6, 2024
-
-    - Added line 118 - 119
-	
-   End of Changes*/
 ?>
---
+++ Karl/calendarsync.php
@@ -0,0 +1,134 @@
+<?php 
+// Include Google calendar api handler class 
+include_once 'calendarapi.class.php'; 
+     
+// Include database configuration file 
+require_once './db_connect.php'; 
+ 
+$statusMsg = ''; 
+$status = 'danger'; 
+if(isset($_GET['code'])){ 
+    // Initialize Google Calendar API class 
+    $GoogleCalendarApi = new GoogleCalendarApi(); 
+     
+    // Get event ID from session 
+    $event_id = $_SESSION['last_event_id']; 
+ 
+    if(!empty($event_id)){ 
+         
+        // Fetch event details from database 
+        $sqlQ = "SELECT * FROM events WHERE id = ?"; 
+        $stmt = $conn->prepare($sqlQ);  
+        $stmt->bind_param("i", $db_event_id); 
+        $db_event_id = $event_id; 
+        $stmt->execute(); 
+        $result = $stmt->get_result(); 
+        $eventData = $result->fetch_assoc(); 
+
+        if(!empty($eventData)){ 
+            $calendar_event = array( 
+                'summary' => $eventData['title'], 
+                'location' => $eventData['location'], 
+                'description' => $eventData['description'], 
+                'attendees' => array($eventData['user_email'], $eventData['counselor_email']),
+                'reminders' => array(
+                    'useDefault' => FALSE,
+                    'overrides' => array(
+                        array('method' => 'email', 'minutes' => 24 * 60),
+                        array('method' => 'popup', 'minutes' => 10),
+                    ),
+                )
+            );
+             
+            $event_datetime = array( 
+                'event_date' => $eventData['date'], 
+                'start_time' => $eventData['time_from'], 
+                'end_time' => $eventData['time_to'] 
+            ); 
+
+            // Get the access token 
+            $access_token_sess = $_SESSION['google_access_token']; 
+            if(!empty($access_token_sess)){ 
+                $access_token = $access_token_sess; 
+            }else{ 
+                $data = $GoogleCalendarApi->GetAccessToken(GOOGLE_CLIENT_ID, REDIRECT_URI, GOOGLE_CLIENT_SECRET, $_GET['code']); 
+                $access_token = $data['access_token']; 
+                $_SESSION['google_access_token'] = $access_token; 
+            } 
+             
+            if(!empty($access_token)){ 
+                try { 
+                    // Delete existing Google Calendar event if exists
+                    if (!empty($eventData['google_calendar_event_id'])) {
+                        $GoogleCalendarApi->DeleteCalendarEvent($access_token, 'primary', $eventData['google_calendar_event_id']);
+                        // echo '<script>alert("1")</script>';
+
+                        $sqlQ = " UPDATE events SET location = NULL, google_calendar_event_id = NULL WHERE id = ?"; 
+                        $stmt = $conn->prepare($sqlQ);  
+                        $stmt->bind_param("i", $db_event_id); 
+                        $db_event_id = $event_id; 
+                        $stmt->execute(); 
+
+                        echo '<script type="text/javascript">
+                        setTimeout(function() {
+                            window.location.href = "index.php?page=appointments/pendingappointments";
+                        }, 0000); 
+                        </script>';
+                        exit();
+                    }
+                    // echo '<script>alert("2")</script>'; 
+
+                    // Get the user's calendar timezone 
+                    $user_timezone = $GoogleCalendarApi->GetUserCalendarTimezone($access_token); 
+                
+                    $google_event_id = $GoogleCalendarApi->CreateCalendarEvent($access_token, 'primary', $calendar_event, 0, $event_datetime, $user_timezone); 
+                     
+                    if($google_event_id){ 
+                        // Update google event reference in the database 
+                        $sqlQ = "UPDATE events SET google_calendar_event_id=? WHERE id=?"; 
+                        $stmt = $conn->prepare($sqlQ); 
+                        $stmt->bind_param("si", $db_google_event_id, $db_event_id); 
+                        $db_google_event_id = $google_event_id; 
+                        $db_event_id = $event_id; 
+                        $update = $stmt->execute(); 
+                         
+                        unset($_SESSION['last_event_id']); 
+                        unset($_SESSION['google_access_token']); 
+                         
+                        $status = 'success'; 
+                        $statusMsg = '<p>Event #'.$event_id.' has been added to Google Calendar successfully!</p>'; 
+                        $statusMsg .= '<p><a href="https://calendar.google.com/calendar/" target="_blank">Open Calendar</a></p>'; 
+
+                        echo '<script type="text/javascript">
+                        setTimeout(function() {
+                            window.location.href = "index.php?page=appointments/pendingappointments";
+                        }, 0000); 
+                        </script>';
+
+                    } 
+                } catch(Exception $e) { 
+                    $statusMsg = $e->getMessage(); 
+                } 
+            }else{ 
+                $statusMsg = 'Failed to fetch access token!'; 
+            } 
+        }else{ 
+            $statusMsg = 'Event data not found!'; 
+        } 
+    }else{ 
+        $statusMsg = 'Event reference not found!'; 
+    } 
+     
+    $_SESSION['status_response'] = array('status' => $status, 'status_msg' => $statusMsg); 
+     
+    exit(); 
+} 
+?>
+
+<script type="text/javascript">
+    function redirectAfterDelay(url, delay) {
+        setTimeout(function() {
+            window.location.href = url;
+        }, delay);
+    }
+</script>
